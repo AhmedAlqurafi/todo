4cffbef3cda9d0a3044ca32cc84a4755e7196036
Ahmed Alqurafi <Ahmed.Alqurafi@gmail.com>
2025-08-09 17:05:29 +0300

    add login functionality

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { InputFieldComponent } from '../../sharable/input-field/input-field.component';
+import { PrimaryButtonComponent } from '../../sharable/primary-button/primary-button.component';
+import { provideIcons } from '@ng-icons/core';
+import {
+  mynaLockSolid,
+  mynaPasswordSolid,
+  mynaUserSolid,
+} from '@ng-icons/mynaui/solid';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [InputFieldComponent, PrimaryButtonComponent, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  viewProviders: [provideIcons({ mynaUserSolid, mynaLockSolid })],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
